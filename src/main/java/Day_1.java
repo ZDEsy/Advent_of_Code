74d5d71ca4c2fa475273df863e6644d42592899e
ZDEsy <zdenekut@gmail.com>
2023-12-01 09:06:34 +0100

    All_files

---
+++ src/main/java/Day_1.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day_1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input.txt"));
+        int sum = 0;
+        while(sc.hasNextLine())
+        {
+            String line = sc.nextLine();
+            line = line.replaceAll("[^\\d.]", "");
+            if(line.length() == 1)
+            {
+                line += line;
+                sum += Integer.parseInt(line);
+            } else if (line.length() > 2) {
+                line = line.charAt(0) + line.substring(line.length() - 1);
+                sum += Integer.parseInt(line);
+            }
+            else
+            {
+                sum += Integer.parseInt(line);
+            }
+        }
+        System.out.println(sum);
+    }
+
+
+}
