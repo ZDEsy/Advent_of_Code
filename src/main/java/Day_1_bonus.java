31ec559d9b5d18b2eea293230772739909ba5212
ZDEsy <zdenekut@gmail.com>
2023-12-02 10:38:49 +0100

    Day_1.java

--- src/main/java/Day_1_bonus.java
+++ src/main/java/Day_1_bonus.java
@@ -9,31 +9,39 @@ public class Day_1_bonus {
         while(sc.hasNextLine())
         {
             String line = sc.nextLine();
-            line = line.replaceAll("one", String.valueOf(1));
-            line = line.replaceAll("two", String.valueOf(2));
-            line = line.replaceAll("three", String.valueOf(3));
-            line = line.replaceAll("four", String.valueOf(4));
-            line = line.replaceAll("five", String.valueOf(5));
-            line = line.replaceAll("six", String.valueOf(6));
-            line = line.replaceAll("seven", String.valueOf(7));
-            line = line.replaceAll("eight", String.valueOf(8));
-            line = line.replaceAll("nine", String.valueOf(9));
-            line = line.replaceAll("[^\\d.]", "");
-            System.out.println(line);
+            String leftNum = "";
+            int index = 0;
+            while (leftNum.isEmpty()) {
+                String s = line.substring(0, index);
+                leftNum = findNum(line.substring(0, index));
+                index++;
+            }
+            String rightNum = "";
+            index = 0;
+            while (rightNum.isEmpty()) {
+                rightNum = findNum(line.substring(line.length() - index));
+                index++;
+            }
 
-            if(line.length() == 1)
-            {
-                line += line;
-                sum += Integer.parseInt(line);
-            } else if (line.length() > 2) {
-                line = line.charAt(0) + line.substring(line.length() - 1);
-                sum += Integer.parseInt(line);
-            }
-            else
-            {
-                sum += Integer.parseInt(line);
-            }
+
+            line = leftNum + rightNum;
+            sum += Integer.parseInt(line);
         }
         System.out.println(sum);
     }
+
+    private static String findNum(String subString) {
+        subString = subString.replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9")
+                .replaceAll("[^\\d.]", "");
+        return subString;
+    }
+
 }
