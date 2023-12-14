b004ddf3f314aac9d776636073c51990a4945ee8
ZDEsy <zdenekut@gmail.com>
2023-12-14 19:15:18 +0100

    Day_5_base_done_but_no_optimalization

--- src/main/java/Day_5.java
+++ src/main/java/Day_5.java
@@ -3,36 +3,54 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Day_5 {
     public static void main(String[] args) throws IOException {
         List<String> strings = Files.readAllLines(Path.of("input5.txt"));
         String[] seeds = strings.get(0).replaceAll("[^\\d.]+", " ").trim().split(" ");
         long[] longSeeds = new long[seeds.length];
-        long diff;
-        long num;
+        long source;
+        long finalSource = 999999999;
         for(int i = 0; i < seeds.length; i++)
         {
             longSeeds[i] = Long.parseLong(seeds[i]);
         }
         System.out.println(Arrays.toString(longSeeds));
-        for (long source : longSeeds) {
-            for (int j = 2; j < strings.size(); j++) {
-                if (strings.get(j).isEmpty()) j++;
-                if (Character.isDigit(strings.get(j).charAt(0))) {
-                    String[] srcDest = strings.get(j).trim().split(" ");
-                    diff = Long.parseLong(srcDest[1]) - Long.parseLong(srcDest[0]);
-                    for (int k = 1; k < Integer.parseInt(srcDest[2]); k++) {
-                        num = Long.parseLong(srcDest[1]) + k;
-                        if (num == source) {
-                            source = num;
-                            break;
+        for (long seed : longSeeds) {
+            source = seed;
+            long plusNum;
+            long tempSource;
+            long tempDest;
+            int i = 3;
+            while(i < 190)
+            {
+                if(strings.get(i).isEmpty()) i += 2;
+                String[] line = strings.get(i).split(" ");
+                plusNum = Long.parseLong(line[2]);
+                tempSource = Long.parseLong(line[1]);
+                tempDest = Long.parseLong(line[0]);
+                for(int x = 0; x < plusNum; x++)
+                {
+                    if(source == tempSource)
+                    {
+                        source = tempDest;
+                        if(i < 188){
+                            while(!strings.get(i+1).isEmpty() && i < 188) i++;
                         }
+                        break;
                     }
+                    tempSource += 1;
+                    tempDest += 1;
                 }
+                i++;
             }
-            System.out.println(source);
+            System.out.println("Final of seed: " + source);
+            if(finalSource > source)
+            {
+                finalSource = source;
+                System.out.println("Found lower: " + source);
+            }
         }
-
     }
 }
